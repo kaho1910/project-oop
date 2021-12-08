e448cfe91d2f1a87b32cffb216aa23fe78146b0d
Trda092 <73659917+Trda092@users.noreply.github.com>
2021-12-08 12:39:53 +0700

    install
    fxml and fx extension

---
+++ Snake_Crazy/src/main/java/com/example/snake_crazy/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.snake_crazy;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
