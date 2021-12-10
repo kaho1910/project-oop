04e714db7fa0ccefde806878e464df66bde6bbb5
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-12-10 14:07:29 +0700

    fix: character pic path

--- src/main/java/game/CheckMotion.java
+++ src/main/java/game/CheckMotion.java
@@ -4,24 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CheckMotion {
-    private HashMap<String,String> motion = new HashMap<String,String>();
-    motion.put("giftofdev", "sad");
-    motion.put("cool", "sad");
-    motion.put("cri", "normal");
-    motion.put("sis", "normal");
-    motion.put("exchange", "sad");
-    motion.put("steal", "sad");
-    motion.put("skip", "sad");
-    motion.put("boom", "sad");
-    motion.put("catwalk", "happy");
-    motion.put("moonwalk", "sad");
-    motion.put("twice", "happy");
-    motion.put("halfhalf", "sad");
-    motion.put("stonk", "happy");
-    motion.put("stink", "sad");
-    motion.put("snakehead", "normal");
-    motion.put("angel", "happy");
-    public String remotion(String cardname){
-        return motion.get(cardname);
-    }
+//    private HashMap<String,String> motion = new HashMap<String,String>();
+//    motion.put("giftofdev", "sad");
+//    motion.put("cool", "sad");
+//    motion.put("cri", "normal");
+//    motion.put("sis", "normal");
+//    motion.put("exchange", "sad");
+//    motion.put("steal", "sad");
+//    motion.put("skip", "sad");
+//    motion.put("boom", "sad");
+//    motion.put("catwalk", "happy");
+//    motion.put("moonwalk", "sad");
+//    motion.put("twice", "happy");
+//    motion.put("halfhalf", "sad");
+//    motion.put("stonk", "happy");
+//    motion.put("stink", "sad");
+//    motion.put("snakehead", "normal");
+//    motion.put("angel", "happy");
+//    public String remotion(String cardname){
+//        return motion.get(cardname);
+//    }
 }
