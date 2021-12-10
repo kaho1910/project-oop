f347bfa9bb7a9234d9c29248cf0fa8aa389d7404
Trda092 <73659917+Trda092@users.noreply.github.com>
2021-12-10 14:24:53 +0700

    check motion

--- src/main/java/game/CheckMotion.java
+++ src/main/java/game/CheckMotion.java
@@ -1,27 +1,27 @@
 package game;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map;
 
 public class CheckMotion {
-    private HashMap<String,String> motion = new HashMap<String,String>();
-    motion.put("giftofdev", "sad");
-    motion.put("cool", "sad");
-    motion.put("cri", "normal");
-    motion.put("sis", "normal");
-    motion.put("exchange", "sad");
-    motion.put("steal", "sad");
-    motion.put("skip", "sad");
-    motion.put("boom", "sad");
-    motion.put("catwalk", "happy");
-    motion.put("moonwalk", "sad");
-    motion.put("twice", "happy");
-    motion.put("halfhalf", "sad");
-    motion.put("stonk", "happy");
-    motion.put("stink", "sad");
-    motion.put("snakehead", "normal");
-    motion.put("angel", "happy");
-    public String remotion(String cardname){
-        return motion.get(cardname);
+    public static void main(String[] args) {
+        HashMap<String,String> motion = new HashMap<String,String>();
+        motion.put("giftofdev", "sad");
+        motion.put("cool", "sad");
+        motion.put("cri", "normal");
+        motion.put("sis", "normal");
+        motion.put("exchange", "sad");
+        motion.put("steal", "sad");
+        motion.put("skip", "sad");
+        motion.put("boom", "sad");
+        motion.put("catwalk", "happy");
+        motion.put("moonwalk", "sad");
+        motion.put("twice", "happy");
+        motion.put("halfhalf", "sad");
+        motion.put("stonk", "happy");
+        motion.put("stink", "sad");
+        motion.put("snakehead", "normal");
+        motion.put("angel", "happy");
     }
+
 }
--- src/main/java/game/PlayerTable.java
+++ src/main/java/game/PlayerTable.java
@@ -50,7 +50,7 @@ public class PlayerTable {
         inner_frame.setHeight(120);
         inner_frame.setTranslateX(0);
         inner_frame.setTranslateY(-99);
-        Image im2 = new Image(getClass().getResourceAsStream("/img/1-normal.png"));
+        Image im2 = new Image(getClass().getResourceAsStream("/img/"+id+"-normal.png"));
         inner_frame.setFill(new ImagePattern(im2));
         ///dice
         dice_frame.setWidth(80);
