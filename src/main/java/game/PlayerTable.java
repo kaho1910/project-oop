092c3dfb9a2c9e67cb49111579f703d46dc8ad94
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-12-11 11:34:21 +0700

    style: clean code

--- src/main/java/game/PlayerTable.java
+++ src/main/java/game/PlayerTable.java
@@ -1,6 +1,5 @@
 package game;
 
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
@@ -11,10 +10,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.*;
 
-
 import java.util.Random;
-
-
 
 public class PlayerTable {
     private int cardNumMax = 3;
@@ -28,7 +24,6 @@ public class PlayerTable {
     private Rectangle[] cardFrame;
     private String[] cardUrl;
     private Image[] cardImg;
-//    private String cardUrl1, cardUrl2, cardUrl3;
     
     public PlayerTable(int id) {
         this.ID = id;
@@ -41,10 +36,6 @@ public class PlayerTable {
         cardFrame = new Rectangle[3];
         cardUrl = new String[cardNumMax];
         cardImg = new Image[cardNumMax];
-//        cardUrl1 = new String();
-//        cardUrl2 = new String();
-//        cardUrl3 = new String();
-
         random = new Random();
 
         player_frame.setWidth(170);
@@ -100,12 +91,6 @@ public class PlayerTable {
                 thread.start();
             }
         });
-//        cardUrl1 = "/img/cards/giftofdev.jpg";
-//        cardUrl2 = "/img/cards/giftofdev.jpg";
-//        cardUrl3 = "/img/cards/giftofdev.jpg";
-//        Image cardImg1 = new Image(getClass().getResourceAsStream(cardUrl1));
-//        Image cardImg2 = new Image(getClass().getResourceAsStream(cardUrl2));
-//        Image cardImg3 = new Image(getClass().getResourceAsStream(cardUrl3));
         for(int i=0; i < cardNumMax; i++){
             cardFrame[i] = new Rectangle();
             cardFrame[i].setWidth(60);
@@ -115,66 +100,11 @@ public class PlayerTable {
             cardUrl[i] = "/img/cards/giftofdev.jpg";
         }
         updateCard();
-//        cardFrame[0].setFill(new ImagePattern(cardImg1));
-//        cardFrame[1].setFill(new ImagePattern(cardImg2));
-//        cardFrame[2].setFill(new ImagePattern(cardImg3));
-
-        //card frame
-//        card_frame.setWidth(60);
-//        card_frame.setHeight(72);
-//        card_frame.setTranslateX(-20);
-//        card_frame.setTranslateY(90);
-//        this.setUrl1("/img/cards/giftofdev.jpg");
-//        Image cm = new Image(getClass().getResourceAsStream(url1));
-//        card_frame.setFill(new ImagePattern(cm));
-//        card_frame.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent mouseEvent) {
-////                System.out.println(url1);
-//                CardPopup.display(url1);
-//            }
-//        });
-        //card frame2
-//        card_frame2.setWidth(60);
-//        card_frame2.setHeight(72);
-//        card_frame2.setTranslateX(60);
-//        card_frame2.setTranslateY(90);
-//        this.setUrl2("/img/cards/giftofdev.jpg");
-//        Image ck = new Image(getClass().getResourceAsStream(url2));
-//        card_frame2.setFill(new ImagePattern(ck));
-//        card_frame2.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent mouseEvent) {
-////                System.out.println(url);
-//                CardPopup.display(url2);
-//            }
-//        });
-        //card frame3
-//        card_frame3.setWidth(60);
-//        card_frame3.setHeight(72);
-//        card_frame3.setTranslateX(140);
-//        card_frame3.setTranslateY(90);
-//        this.setUrl3("/img/cards/giftofdev.jpg");
-//        Image cl = new Image(getClass().getResourceAsStream(url3));
-//        card_frame3.setFill(new ImagePattern(cl));
-//        card_frame3.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent mouseEvent) {
-////                System.out.println(url);
-//                CardPopup.display(url3);
-//            }
-//        });
         layout.getChildren().addAll(player_frame,inner_frame, dice_frame, diceButton);
         for (Rectangle i:
              cardFrame) {
             layout.getChildren().add(i);
         }
-//        scene = new Scene(layout, 400 ,400);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//    public static void main(String[] args) {
-//        launch();
     }
 
     public void updateCard(){
