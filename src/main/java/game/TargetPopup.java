49ca7ec501d5a5327d6cb728603145ef45d18165
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-12-11 15:58:43 +0700

    style: targetPopUp

--- src/main/java/game/TargetPopup.java
+++ src/main/java/game/TargetPopup.java
@@ -1,15 +1,9 @@
 package game;
 
-import javafx.animation.PauseTransition;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.ImagePattern;
@@ -17,72 +11,72 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.application.*;
-import javafx.util.Duration;
 
 public class TargetPopup extends Application{
-    private Stage popUpStage;
     private Button[] pbutton;
-    private PlayerTable[] player;
+    private Player[] player;
     private Image profile[], frame[];
     private Rectangle framebox[], innerframebox[];
     private int selected,i=0, j;
     public void start(Stage popUpStage){
-        //popUpStage = new Stage();
         StackPane select_pane = new StackPane();
-        player = new PlayerTable[6];
-        profile = new Image[6]; frame = new Image[6];
-        framebox = new Rectangle[6]; innerframebox = new Rectangle[6];
-        pbutton = new Button[6];
-        for (i =1; i<=4;i++){
-        pbutton[i] = new Button();
-        player[i] = new PlayerTable(i);
-        profile[i] = player[i].getIm2(); frame[i] = player[i].getIm1();
-        framebox[i] = new Rectangle(); innerframebox[i] = new Rectangle();
-        pbutton[i].setText("select");
-        framebox[i].setHeight(170);
-        framebox[i].setWidth(170);
-        framebox[i].setFill(new ImagePattern(frame[i]));
-        innerframebox[i].setHeight(120);
-        innerframebox[i].setWidth(120);
-        innerframebox[i].setFill(new ImagePattern(profile[i]));
+        player = new Player[4];
+        profile = new Image[4];
+        frame = new Image[4];
+        framebox = new Rectangle[4];
+        innerframebox = new Rectangle[4];
+        pbutton = new Button[4];
+        for (i=0; i<=3;i++){
+            player[i] = new Player(i + 1);
+            pbutton[i] = new Button();
+            profile[i] = player[i].getPlayerTable().getIm2();
+            frame[i] = player[i].getPlayerTable().getIm1();
+            framebox[i] = new Rectangle();
+            innerframebox[i] = new Rectangle();
+            pbutton[i].setText("select");
+            framebox[i].setHeight(170);
+            framebox[i].setWidth(170);
+            framebox[i].setFill(new ImagePattern(frame[i]));
+            innerframebox[i].setHeight(120);
+            innerframebox[i].setWidth(120);
+            innerframebox[i].setFill(new ImagePattern(profile[i]));
 
-        if (i%2 ==0){
-            framebox[i].setTranslateX(200);
-            innerframebox[i].setTranslateX(200);
-            pbutton[i].setTranslateX(200);
-            //framebox[i].setTranslateY();
+            if ((i + 1)%2 ==0){
+                framebox[i].setTranslateX(200);
+                innerframebox[i].setTranslateX(200);
+                pbutton[i].setTranslateX(200);
+                //framebox[i].setTranslateY();
+            }
+            else{
+                framebox[i].setTranslateX(-200);
+                innerframebox[i].setTranslateX(-200);
+                pbutton[i].setTranslateX(-200);
+            }
+            if ((i + 1) / 3 < 1){
+                framebox[i].setTranslateY(-150);
+                innerframebox[i].setTranslateY(-150);
+                pbutton[i].setTranslateY(-30);
+            }
+            else{
+                framebox[i].setTranslateY(100);
+                innerframebox[i].setTranslateY(100);
+                pbutton[i].setTranslateY(220);
+            }
+            select_pane.getChildren().addAll(framebox[i], innerframebox[i], pbutton[i]);
         }
-        else{
-            framebox[i].setTranslateX(-200);
-            innerframebox[i].setTranslateX(-200);
-            pbutton[i].setTranslateX(-200);
-        }
-        if (i/3 < 1){
-            framebox[i].setTranslateY(-150);
-            innerframebox[i].setTranslateY(-150);
-            pbutton[i].setTranslateY(-30);
-        }
-        else{
-            framebox[i].setTranslateY(100);
-            innerframebox[i].setTranslateY(100);
-            pbutton[i].setTranslateY(220);
-        }
-        select_pane.getChildren().addAll(framebox[i], innerframebox[i], pbutton[i]);
-        }
-        for (j = 1;j<=4;j++) {
+        for (j=0;j<=3;j++) {
             pbutton[j].setOnMouseClicked(new EventHandler<MouseEvent>() {
                 String s = new String(pbutton[j].toString());
                 int num = j;
                 @Override
                 public void handle(MouseEvent mouseEvent) {
-                    System.out.println(mouseEvent.getSource().toString().equals(s));
+//                    System.out.println(mouseEvent.getSource().toString().equals(s));
                     if (mouseEvent.getSource().toString().equals(s)){
-                        for (int k=1;k<=4;k++){
+                        for (int k=0;k<=3;k++){
                             pbutton[k].setDisable(true);
                         }
                         selected = num;
-                        System.out.println(selected);
-
+                        System.out.println("Target: " + selected);
                     }
                 }
             });
@@ -106,10 +100,4 @@ public class TargetPopup extends Application{
     public static void main(String[] args) {
         launch();
     }
-
-
-
-//    public static Stage getPopUpStage() {
-//        return popUpStage;
-//    }
 }
