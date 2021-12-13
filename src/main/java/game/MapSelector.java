56df16262db30fc717c64de7e9fb9cfa32702d87
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-12-13 21:40:29 +0700

    fix: map1 Ladder

--- src/main/java/game/MapSelector.java
+++ src/main/java/game/MapSelector.java
@@ -23,7 +23,7 @@ public class MapSelector {
     private Button[] btn = new Button[mapNum];
     private String[] mapName = {"Yume no Taiki", "Judai in The City", "Yuji on Train"};
 
-    private int[][] map1Ladder = {{14, 7}, {16, 25}, {32, 49}, {60, 42}, {46, 66}, {89, 72}, {77, 84}, {98, 78}};
+    private int[][] map1Ladder = {{14, 7}, {16, 25}, {32, 49}, {60, 41}, {46, 66}, {89, 72}, {77, 84}, {98, 78}};
     private int[][] map2Ladder = {{3, 18}, {30, 11}, {22, 37}, {66, 46}, {80, 61}, {90, 73}, {74, 87}, {77, 97}};
     private int[][] map3Ladder = {{18, 3}, {11, 30}, {25, 36}, {41, 61}, {68, 53}, {63, 78}, {92, 72}, {99, 82}};
 
